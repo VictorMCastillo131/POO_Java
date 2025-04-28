4c158faf5fbcf84705d5127579090ee95c86c5e7
Victor Manuel Castillo Duran <victormanuelcastilloduran02@gmail.com>
2025-04-27 21:37:35 -0600

    Add files via upload

---
+++ Canicas2.java
@@ -0,0 +1,21 @@
+package Unidad2;
+
+public class Canicas2 {
+    public void imprimirPorCanicas( ) {
+        short total = 158;
+        float r = (int) (total * 13.0 / 100);
+        System.out.println("Las canicas rojas que tiene Carlitos son: " + r);
+        float a = (int) (total * 22.0 / 100);
+        System.out.println("Las canicas azules que tiene Carlitos son: " + a);
+        float ag = (int) (total * 10.0 / 100);
+        System.out.println("Las canicas aguitas que tiene Carlitos son: " + ag);
+        float pon = (int) (total * 35.0 / 100);
+        System.out.println("Las canicas ponchitos negros que tiene Carlitos son: " + pon);
+        float bon = (int) (total * 20.0 / 100);
+        System.out.println("Las canicas bonbochas que tiene Carlitos son: " + bon);
+    }
+}
+      
+                                                                  
+ 
+ 
